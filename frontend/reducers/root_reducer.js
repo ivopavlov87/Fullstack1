7788a421227bf72ecbeec61c4fcbdd8c02ba6fa3
ivopavlov <pavlov.ivo@gmail.com>
2019-09-02 15:29:23 -0700

    Spash now works, user auth needs implementation

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,15 +1,7 @@
 import { combineReducers } from 'redux';
 
-import entities from './entities_reducer'; // NEED TO IMPLEMENT
-import ui from './ui_reducer'; // NEED TO IMPLEMENT
-import session from './session_reducer'; // NEED TO IMPLEMENT
-import errors from './errors_reducer'; // NEED TO IMPLEMENT
-
 const rootReducer = combineReducers({
-  entities,
-  session,
-  ui,
-  errors,
+
 });
 
 export default rootReducer;
