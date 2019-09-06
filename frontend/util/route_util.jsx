bd60d9994e81f517e142b7f1acbf15f5fc049a37
ivopavlov <pavlov.ivo@gmail.com>
2019-09-06 08:30:47 -0700

    Landing image hidden when small resolution; need route work

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,14 +1,29 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import { Route, Redirect, withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect, withRouter } from "react-router-dom";
 
-// const Auth = ({ component: Component, path, loggedIn, exact }) => (
-//   <Route path={path} exact={exact} render={(props) => (
-//     !loggedIn ? (<Component {...props} />) : (<Redirect to="/" />))} />
-// );
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
 
-// const mapStateToProps = state => (
-//   {loggedIn: Boolean(state.session.id)}
-// );
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      loggedIn ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
 
-// export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+const mapStateToProps = state => ({ loggedIn: Boolean(state.session.id) });
+
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
